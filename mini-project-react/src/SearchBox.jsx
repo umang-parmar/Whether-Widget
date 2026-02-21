b33cacbf4bbd6cf9277fcf1c352ee6c2ad41fa4c
umang-parmar <umang2502parmar@gmail.com>
2026-02-21 08:06:28 +0530

    Implement Api

--- mini-project-react/src/SearchBox.jsx
+++ mini-project-react/src/SearchBox.jsx
@@ -5,20 +5,40 @@ import { useState } from "react";
 
 export default function SearchBox(){
     let [city,setCity] = useState("");
+    const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+    const API_KEY = "6f6210a72718e9262df6c0b07f638684";
+
+    let getWhetherInfo = async () => {
+      let responce = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+      let jsonResponce=await responce.json();
+      let result={
+        temp: jsonResponce.main.temp,
+        tempMin: jsonResponce.main.temp_min,
+        tempMax: jsonResponce.main.temp_max,
+        humidity:jsonResponce.main.humidity,
+        feelsLike:jsonResponce.main.feels_like,
+        whether:jsonResponce.weather[0].description,
+      };
+      console.log(result);
+      
+    }; 
 
     let handleChange = (evt) => {
         setCity(evt.target.value);
-        console.log("yyyyyyyyyy");
     };
 
     let handleSubmit = (evt) => {
-        console.log("ggggg");
+        evt.preventDefault();
+        console.log(city);
+        setCity("");
+        getWhetherInfo();
+
         
     }
     return(
         <div className="SearchBox">
             <h3>Search For The Whether</h3>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <TextField 
                     id="city"
                     label="City Name"
