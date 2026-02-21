ba12948827944764909919abe3a8a87cb2c2b962
umang-parmar <umang2502parmar@gmail.com>
2026-02-21 11:30:54 +0530

    Adding Some Feauters , Icons ,

--- mini-project-react/src/InfoBox.jsx
+++ mini-project-react/src/InfoBox.jsx
@@ -4,21 +4,38 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import "./InfoBox.css"; 
 
+import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+
 export default function InfoBox({ info }){
 const INIT_URL = "https://plus.unsplash.com/premium_photo-1729600377083-bbe558d8b7a6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
+const HOT_URL="https://media.istockphoto.com/id/1370821565/photo/extreme-weather-and-climate-change.webp?a=1&b=1&s=612x612&w=0&k=20&c=wvx5BnKCpNESpT-qSsUtDuxnoiDhH4QpqBuOPdnB8DM=";
+const COLD_URL="https://images.unsplash.com/photo-1640942824064-6209c10b9091?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGNvbGQlMjB3ZWF0aGVyJTIwaW1hZ2V8ZW58MHx8MHx8fDA%3D";
+const RAIN_URL="https://media.istockphoto.com/id/2166330742/photo/mumbai-monsoon-man.webp?a=1&b=1&s=612x612&w=0&k=20&c=XJBghA5CIenFGXTSGy62e2xAplxVCJOjj_39h2FKUWs=";
  return( 
     <div className="InfoBox">
        <h1>WhetherInfo -{info.weather}</h1>
          <Card sx={{ maxWidth: 345 }}>
       <CardMedia
         sx={{ height: 140 }}
-        image={INIT_URL}
+        image={info.humidity > 80 
+                ? RAIN_URL 
+                : (info.temp > 15) 
+                    ? HOT_URL 
+                    : COLD_URL
+              }
         title="green iguana"
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          {info.city}
+            {info.city}    {info.humidity > 80 
+                            ? <ThunderstormIcon/> 
+                            : (info.temp > 15) 
+                                ? <WbSunnyIcon/> 
+                                : <AcUnitIcon/>
+                            }
         </Typography>
         <Typography variant="body2" sx={{ color: 'text.secondary' }}component={"span"}>
             <div>Temperature = {info.temp}&deg;C</div> <br></br>
